7ffea23e93fc1b26237a4bbbe31d4cf2bacde04b
Den_Slav <joomlakop@gmail.com>
2021-12-31 20:42:45 +0200

    add plugin

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ const less = require('gulp-less');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 const cleanCss = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');//Для подержки старіх версий браузеров
 const del = require('del');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');//Обьеденение файлов
 const uglify = require('gulp-uglify');//Минификация JS
 const imagemin = require ('gulp-imagemin');
 const newer = require('gulp-newer');
+const sourcemaps = require('gulp-sourcemaps');//карта css файла
 
 /*Настраиваем пути к файлам разработки и конечным файлам*/
 const paths = {
@@ -30,7 +31,7 @@ const paths = {
         dest: 'dist/js/' //Путь к конечному файлу
     },
     image: {
-        src: 'src/img/**/*.*', //Путь к файлу разработки 
+        src: 'src/img/*', //Путь к файлу разработки 
         dest: 'dist/img/' //Путь к конечному файлу
     }
 }
@@ -49,6 +50,7 @@ function html(){
 /*Функция компиляции less в css*/
 function stylesLess() {
     return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 8 versions']
@@ -58,12 +60,14 @@ function stylesLess() {
         basename: 'main',
         suffix: '.min'
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest))
 };
 
 /*Функция компиляции sass в css*/
 function stylesSass() {
     return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 8 versions']
@@ -73,6 +77,7 @@ function stylesSass() {
         basename: 'main',
         suffix: '.min'
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest))
 };
 
@@ -81,9 +86,13 @@ function scripts(){
     return gulp.src(paths.scripts.src, {
         sourcemaps: true
     })
-    .pipe(babel())
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest))
 }
 
